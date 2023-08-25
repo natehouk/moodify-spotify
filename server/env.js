25024fe67cf6d3b1f6b9a565ae3a59671cc78f5b
Nate Houk <natehouk@users.noreply.github.com>
2023-08-25 02:51:05 +0200

    Update env.js

--- server/env.js
+++ server/env.js
@@ -1,4 +1,4 @@
-const clientRoot = (process.env.NODE_ENV === 'production') ? 'https://moodify.ca' : 'http://localhost:3000';
+const clientRoot = (process.env.NODE_ENV === 'production') ? 'https://zingy-cranachan-bde3f4.netlify.app' : 'http://localhost:3000';
 const serverRoot = (process.env.NODE_ENV === 'production') ? 'https://moodify.ca/c' : 'http://localhost:9000';
 
 module.exports = { clientRoot, serverRoot };
